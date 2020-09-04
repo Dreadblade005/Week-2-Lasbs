108c8f8fa9b96bf9d1f024041c79bc250dc65149
dread <dread@DESKTOP-BS1AKPM>
2020-09-03 20:03:57 -0700

    Add project files.

---
+++ Lecture 2 lab/Program.cs
@@ -0,0 +1,62 @@
+﻿/// Week 2		Lab No. 1
+/// File Name: MyFirstProgram.java
+/// @author: Dakota Durst
+/// Date:  September 3rd, 2020
+///
+/// Problem Statement: Ask the user to enter three numbers, calculate the sum of
+/// these numbers, and display the sum to the screen
+/// 
+/// 
+/// Overall Plan:
+/// 1) Edit message to commadate extra integer
+/// 2) create 3 integers and sum
+/// 3) get user input 3 seperate times to get 3 variables
+/// 4) take 3 numbers and add them together, display result
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class MyFirstProgram
+    {
+        static void Main(string[] args)
+        {
+            // print a message to the screen
+            Console.WriteLine("Hello out there.");
+            Console.WriteLine("I will add three numbers for you.");
+            Console.WriteLine("Enter one number and press enter, then enter second number and press enter, and then lastly enter a third number and press enter:");
+
+            // declare all integer variables
+            int n1, n2, n3, sum; 
+            double do_math, times;
+
+
+            n1 = Int32.Parse(Console.ReadLine());
+            n2 = Int32.Parse(Console.ReadLine());
+            n3 = Int32.Parse(Console.ReadLine());
+
+
+            // calculate the sum, product, and percentage of these two numbers
+            sum = n1 + n2 + n3;
+            times = n1 * n2 * n3; 
+            do_math = sum / times;
+
+            // print a message to make sure the math is correct
+            Console.WriteLine("The sum of those three numbers is: " + sum);
+            Console.WriteLine("The product of those three numbers is: " + times);
+            Console.WriteLine("The sum divided by the product is equal to: " + do_math);
+
+
+
+            //Just to pause the screen.
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Part 2/Program.cs
@@ -0,0 +1,57 @@
+﻿/// File Name: MyFirstProgram.java
+/// @author: Dakota Durst
+/// Date:  September 3rd, 2020
+///
+/// Problem Statement: Take a number of coupons and divide them among candy and gumballs
+/// 
+/// 
+/// Overall Plan:
+/// 1) assign variables: coupons, cost of candy/gum, coupins after candy/gum
+/// 2) take coupons and divide coupons by cost of gum
+/// 3) assign the value of quotient to the number of candy bars, take remainder as new number of currently held coupons
+/// 4) repeat step 3 with gum instead of candy
+/// 5) print out amount of candy bars, gum, and remaining coupons
+
+
+
+using System;
+using System.Runtime.ExceptionServices;
+
+namespace Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            int num_coupons = 108;
+            int num_coupons_after_candy = 0;
+            int num_coupons_after_gum = 0;
+            int num_candy_bars = 0;
+            int num_gumballs = 0;
+
+            ///Processing
+            num_candy_bars = num_coupons / 10;
+            //System.out.println("Number candy bars: " + num_candy_bars);
+            //calculate remaining bars
+            num_coupons_after_candy = num_coupons % 10;
+            ////System.out.println("Number of coupons after candy: " + num_coupons_after_candy);
+            //calculate gumballs;
+            num_gumballs = num_coupons_after_candy / 3;
+            //calculate remaining coupons 
+            num_coupons_after_gum = num_coupons_after_candy % 3;
+
+
+            ///output
+            Console.WriteLine("Your " + num_coupons + " coupons can be redeemed for " + num_candy_bars + " candy bars and " + num_gumballs +
+            " gumballs with " + num_coupons_after_gum + " coupons remaining.");
+
+
+
+
+
+
+
+        }
+    }
+}
